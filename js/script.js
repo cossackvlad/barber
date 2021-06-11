7d91ffd27c72fe193187602ad947a3f6bc788211
Vlad <v.kotenko95@gmail.com>
2021-06-11 19:39:01 +0300

    add js script

--- js/script.js
+++ js/script.js
@@ -2,11 +2,16 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       close = document.querySelector('.close');
+      menuLink = document.querySelector('.menu__top-box');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
 
 close.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+menuLink.addEventListener('click', () => {
     menu.classList.remove('active');
 });

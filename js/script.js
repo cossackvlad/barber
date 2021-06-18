d63f7f9cd069474f797c6d532e92702c60ea13ab
Vlad <v.kotenko95@gmail.com>
2021-06-18 10:33:39 +0300

    bag fix

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Menu
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
-      close = document.querySelector('.close');
+      close = document.querySelector('.close'),
       menuLink = document.querySelector('.menu__top-box');
 
 hamburger.addEventListener('click', () => {
@@ -15,3 +15,20 @@ close.addEventListener('click', () => {
 menuLink.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+// Modal
+// const modal = document.querySelector('.btn'),
+//       form = document.querySelector('.form'),
+//       closeForm = document.querySelector('.closeForm');
+
+// modal.addEventListener('click', () => {
+//     form.classList.add('active');
+// });
+
+// closeForm.addEventListener('click', () => {
+//     form.classList.remove('active');
+// });
+
+// close.addEventListener('click', () => {
+//     form.classList.remove('active');
+// });
